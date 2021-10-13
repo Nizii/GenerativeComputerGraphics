bd6372d8f0c0a06dd833b794b93511943ad5945b
Nizii <nizam.oezdemir@gmx.ch>
2021-10-13 17:28:46 +0200

    commit

--- p5js/ext/sketch.js
+++ p5js/ext/sketch.js
@@ -3,7 +3,7 @@ var rectangle = [];
 // count of every frame in draw function
 var drawCount;
 // threshold for the transition draw background to draw houses
-var end;
+var threshold;
 // x coordinates of the rect of the house
 var houseX;
 // y coordinates of the rect of the house
@@ -17,8 +17,8 @@ function setup() {
   fill("yellow");
   houseX = random(0,200);
   houseY = random(0,200);
-  structureHeightCunt = 0;
-  end = 1000;
+  houseHeightCunt = 0;
+  threshold = 1000;
   drawCount = 0;
 }
 
@@ -29,9 +29,8 @@ function draw() {
       rect(rectangle[index].x, rectangle[index].y, rectangle[index].h, rectangle[index].w);
       index += 1;
     }
-    // if frameCount < than 1000 frames draw background
-    // after 1000 loops draw houses
-    if(drawCount < end){
+    // if frameCount < than 1000 frames draw background after 1000 loops draw houses
+    if(drawCount < threshold){
       setBackground();
     }else{
       buildHouse();
@@ -40,21 +39,19 @@ function draw() {
 
 // set rectangel by random for background
 function setBackground() {
-  rectangle[rectangle.length] = { x: random(1, 800), y: random(1, 800) , w: random(1, 80), h:random(1, 80)}
+  rectangle[rectangle.length] = { x: random(1, 850), y: random(1, 850) , w: random(1, 80), h:random(1, 80)}
 }
 
 function buildHouse() {
   houseHeightCunt += 1;
-  // each layer is shifted by certain coordinates,
-  // so that the house looks like a house when you look down from above
+  // each layer is shifted by certain coordinates, so that the house looks like a house when you look down from above
   houseX += random(0,2);
   houseY += random(3,0);
-  // after 29 layers have been laid, 
-  // the roof is put in place as last layer and then the new position of the structures will determined by random
+  // after 29 layers have been laid, the roof is put in place as last layer and then the new position of the structures will determined by random
   if(houseHeightCunt % 30 == 0 && houseHeightCunt < 240){
     rectangle[rectangle.length] = { x: houseX, y: houseY , w: random(80,100), h: random(80,100)}
-    houseX = random(1,800);
-    houseY = random(1,800);
+    houseX = random(1,700);
+    houseY = random(1,700);
   // one layer of a house will laid here
   }else if (houseHeightCunt < 241){
     rectangle[rectangle.length] = { x: houseX, y: houseY , w: random(0, 100), h: random(0, 100)}
